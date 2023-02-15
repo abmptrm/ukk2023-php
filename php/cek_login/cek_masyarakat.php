c33c264ac0b6613d8a5f1586885bcfa5a3a2bb87
unknown <adilbimoadil123@gmail.com>
2023-02-15 09:11:10 +0700

    add cek login masyarakat

--- php/cek_login/cek_masyarakat.php
+++ php/cek_login/cek_masyarakat.php
@@ -3,32 +3,38 @@
 session_start();
 
 // menghubungkan php dengan koneksi database
-include 'koneksi.php';
+include '../koneksi/koneksi.php';
 
-// menangkap data yang dikirim dari form login
-$username = $_POST['username'];
-$password = md5($_POST['password']);
+if (isset($_POST['masuk-masyarakat'])) {
+
+	// menangkap data yang dikirim dari form login
+	$username = $_POST['username'];
+	$password = md5($_POST['password']);
 
 
-// menyeleksi data user dengan username dan password yang sesuai
-$login = mysqli_query($koneksi,"select * from masyarakat where username='$username' and password='$password'");
-// menghitung jumlah data yang ditemukan
-$cek = mysqli_num_rows($login);
+	// menyeleksi data user dengan username dan password yang sesuai
+	$login = mysqli_query($koneksi,"select * from masyarakat where username='$username' and password='$password'");
+	// menghitung jumlah data yang ditemukan
+	$cek = mysqli_num_rows($login);
 
-// cek apakah username dan password di temukan pada database
-if($cek > 0){
+	// cek apakah username dan password di temukan pada database
+	if($cek > 0){
 
-	// buat session login dan username
-	$_SESSION['id_petugas'] = $id_petugas;
-	$_SESSION['username'] = $username;
-	$_SESSION['password'] = $password;
-	$_SESSION['level'] = "admin";
-	// alihkan ke halaman dashboard admin
-	header("location: index.php");
+		$_SESSION['username'] = $username;
+		$_SESSION['nik'] = $nik;
+		$_SESSION['nama'] = $nama;
+		$_SESSION['status'] = "login";
 
-	
-}else{
-	header("location: login.php?info=gagal");
+		header("location: ../../index.html");
+
+		
+	}else{
+		header("location: ../../login.html?info=gagal");
+	}
+
 }
+
+
+
 
 ?>
--- php/cek_login/cek_petugas.php
+++ php/cek_login/cek_petugas.php
@@ -11,50 +11,46 @@ if (isset($_POST['masuk-petugas'])){
 	$username = $_POST['username'];
 	$password = md5($_POST['password']);
 
-	if(!empty($username) || !empty($password)){
+	// menyeleksi data user dengan username dan password yang sesuai
+	$login = mysqli_query($koneksi,"select * from petugas where username='$username' and password='$password'");
+	// menghitung jumlah data yang ditemukan
+	$cek = mysqli_num_rows($login);
 
-		// menyeleksi data user dengan username dan password yang sesuai
-		$login = mysqli_query($koneksi,"select * from petugas where username='$username' and password='$password'");
-		// menghitung jumlah data yang ditemukan
-		$cek = mysqli_num_rows($login);
+	// cek apakah username dan password di temukan pada database
+	if($cek > 0){
 
-		// cek apakah username dan password di temukan pada database
-		if($cek > 0){
+		$data = mysqli_fetch_assoc($login);
 
-			$data = mysqli_fetch_assoc($login);
+		// cek jika user login sebagai admin
+		if($data['level']=="admin"){
 
-			// cek jika user login sebagai admin
-			if($data['level']=="admin"){
+			// buat session login dan username
+			$_SESSION['id_petugas'] = $id_petugas;
+			$_SESSION['username'] = $username;
+			$_SESSION['password'] = $password;
+			$_SESSION['level'] = "admin";
+			// alihkan ke halaman dashboard admin
+			header("location:../../administator/beranda.html");
 
-				// buat session login dan username
-				$_SESSION['id_petugas'] = $id_petugas;
-				$_SESSION['username'] = $username;
-				$_SESSION['password'] = $password;
-				$_SESSION['level'] = "admin";
-				// alihkan ke halaman dashboard admin
-				header("location:../../administator/beranda.html");
+		// cek jika user login sebagai pegawai
+		}else if($data['level']=="petugas"){
+			// buat session login dan username
+			$_SESSION['id_petugas'] = $id_petugas;
+			$_SESSION['username'] = $username;
+			$_SESSION['password'] = $password;
+			$_SESSION['level'] = "petugas";
+			// alihkan ke halaman dashboard pegawai
+			header("location:../../petugas/beranda.html");
 
-			// cek jika user login sebagai pegawai
-			}else if($data['level']=="petugas"){
-				// buat session login dan username
-				$_SESSION['id_petugas'] = $id_petugas;
-				$_SESSION['username'] = $username;
-				$_SESSION['password'] = $password;
-				$_SESSION['level'] = "petugas";
-				// alihkan ke halaman dashboard pegawai
-				header("location:../../petugas/beranda.html");
-
-			}else{
-
-				// alihkan ke halaman login kembali
-				header("location:../../login-petugas.php?info=gagal");
-			}	
 		}else{
-			header("location:../../login-petugas.php?info=gagal");
-		}
 
+			// alihkan ke halaman login kembali
+			header("location:../../login-p.php?info=gagal");
+		}	
+	}else{
+		header("location:../../login-p.php?info=gagal");
 	}
-	
+
 }
 
 
